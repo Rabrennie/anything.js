989f1828544fe86aa4c2672a2bdd3a8c68440fe6
Valentin Gehrke <madmaurice@zom.bi>
2015-12-06 00:04:35 +0100

    Added rot13 functionality.

--- dist/anything.js
+++ dist/anything.js
@@ -692,6 +692,26 @@
     }
 
     anything.prototype.returnArgument = returnArgument;
+    var rot13 = function(str) {
+        var str_rot13 = "";
+        var codeA = "A".charCodeAt(0);
+        var codeZ = "Z".charCodeAt(0);
+        var codea = "a".charCodeAt(0);
+        var codez = "z".charCodeAt(0);
+        for (var i = 0; i < str.length; i++) {
+            c = str.charCodeAt(i);
+            if (c >= codeA && c <= codeZ) {
+                c = codeA + ((c - codeA + 13) % 26);
+            } else if (c >= codea && c <= codez) {
+                c = codea + ((c - codea + 13) % 26);
+            }
+            str_rot13 += String.fromCharCode(c);
+        }
+        return str_rot13;
+    }
+
+    anything.prototype.rot13 = rot13;
+
     // Usage
     //sheet.insertRule("header { float: left; opacity: 0.8; }", 1);
     var sheet = (function() {
@@ -733,10 +753,11 @@
     anything.prototype.sumArray = sumArray;
 
     var theAnswerToLifeTheUniverseAndEverything = function() {
-        return 42
+        return anything.everything();
     };
 
     anything.prototype.theAnswerToLifeTheUniverseAndEverything = theAnswerToLifeTheUniverseAndEverything;
+
     /**
      * Tuple "class".
      */
