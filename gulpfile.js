65d750cbd9e39980d18f860ba48dcd9f4805ba32
Rab Rennie <rab.rennie@planysdigital.com>
2015-12-05 20:59:01 +0000

    added minification to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,17 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var prettify = require('gulp-jsbeautifier');
+var minify = require('gulp-minify');
 
 gulp.task('default', function() {
   return gulp.src(['./src/constants/start.js','./src/*.js','./src/constants/end.js'])
     .pipe(concat('anything.js'))
     .pipe(prettify())
+    .pipe(minify({
+        ext:{
+            src: '.js',
+            min: '.min.js'
+        }
+    }))
     .pipe(gulp.dest('./dist/'));
 });
