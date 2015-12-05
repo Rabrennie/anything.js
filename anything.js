577512819ce615d23327d20f5d086c0c052b5e31
prakashn27 <nprakashece@gmail.com>
2015-12-05 14:03:17 -0500

    added exec function

--- anything.js
+++ anything.js
@@ -426,6 +426,21 @@
         return product;
     }
 
+    /*
+        define a function that takes two arguments and return a single result
+        apply the function to all the argument of the function
+        eg. 
+            function sumNew(a, b) { return a + b; }
+            Δ.execFunction(sumNew, 1, 2, 3, 4) returns 10
+            Δ.execFunction(sumNew, 1, 2, 3, 4, 5) returns 15
+    */
+
+    function execFunction(func) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        var result = args.reduce(func);
+        return result;
+    }
+
     //prototypes go here
     anything.prototype.doTheThing = doTheThing;
     anything.prototype.flipText = flipText;
@@ -461,6 +476,7 @@
     anything.prototype.batman = batman;
     anything.prototype.sum = sum;
     anything.prototype.product = product;
+    anything.prototype.execFunction = execFunction;
 
     //put that shit where everyone can see it.
     if(typeof(window.Δ) === 'undefined'){
