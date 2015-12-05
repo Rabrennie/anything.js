5bc4a15e9b99a527ffa320e3c415cf34972d10dd
Rab Rennie <rab.rennie@planysdigital.com>
2015-12-05 23:38:04 +0000

    updated dist

--- dist/anything.js
+++ dist/anything.js
@@ -697,6 +697,21 @@
 
     anything.prototype.morse = morse;
 
+    var morseBack = function(morseString) {
+        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890@?.,- ";
+        var morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", ".--.-.", "..--..", ".-.-.-", "--..--", "-....-", "   "];
+        var newString = "";
+        morseString.split(" ").forEach(function(a) {
+            var i = morse.indexOf(a);
+            if (a != -1) {
+                newString += chars[i];
+            }
+        });
+        return newString;
+    };
+
+    anything.prototype.morseBack = morseBack;
+
     function negMod(n, m) {
         return ((n % m) + m) % m;
     };
