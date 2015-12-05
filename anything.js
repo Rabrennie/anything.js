a37886eb0a0c8bebf28555804cac65f9736aa73c
Rab Rennie <rab.rennie@planysdigital.com>
2015-12-05 16:05:30 +0000

    added changes by @gytdau
    closes #35

--- anything.js
+++ anything.js
@@ -278,6 +278,44 @@
         return RGBtoCMYK(rgb);
     }
 
+    var g = {
+      build: function(base, shaft, end, tail) {
+        return base + shaft + end + tail
+      },
+      base: {
+        full: "8",
+        half: "3"
+      },
+      shaftType: {
+        double: "=",
+        alternative: "═"
+      },
+      shaftLength: {
+        nano: 0,
+        macro: 1,
+        milli: 2,
+        kilo: 5,
+        mega: 10,
+        giga: 20,
+        tera: 100
+      },
+      shaftBuilder: function(shaft, length) {
+        return Array(length+1).join(shaft);
+      },
+      end: {
+        circle: "D",
+        circleDashed: "‎Ð",
+        pointy: ">",
+        rounded: ")",
+        flat: "]"
+      },
+      tail: {
+        wavy: "~",
+        flat: "-",
+        double: "="
+      }
+    }
+
 
     //prototypes go here
     anything.prototype.doTheThing = doTheThing;
@@ -303,6 +341,7 @@
     anything.prototype.HSLtoRGB = HSLtoRGB;
     anything.prototype.CMYKtoHSL = CMYKtoHSL;
     anything.prototype.HSLtoCMYK = HSLtoCMYK;
+    anything.prototype.g = g;
 
     //put that shit where everyone can see it.
     if(typeof(window.Δ) === 'undefined'){
