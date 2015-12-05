31f3fe6e015ef700aea23c0c5296c846d904f6a0
Rab Rennie <rab.rennie@planysdigital.com>
2015-12-05 21:58:51 +0000

    updated dist

--- dist/anything.js
+++ dist/anything.js
@@ -651,6 +651,22 @@
     }
 
     anything.prototype.sum = sum;
+    /* Sums up and returns all the values in the array passed into the function
+    usage:  sumArray([1,2]) returns 3
+            sumArray([1, 2, 3]) returns 6
+            sumArray([1, 2, 3, 4]) returns 10
+    */
+
+    var sumArray = function(array) {
+        var response = 0;
+        for (i in array) {
+            response += array[i];
+        }
+        return response;
+    }
+
+    anything.prototype.sumArray = sumArray;
+
     var theAnswerToLifeTheUniverseAndEverything = function() {
         return 42
     };
