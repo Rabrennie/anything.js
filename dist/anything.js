7b9647c2919deb86fc744310ec4b11997b6ece1d
Rab Rennie <rab.rennie@planysdigital.com>
2015-12-06 18:17:29 +0000

    updated dist

--- dist/anything.js
+++ dist/anything.js
@@ -746,6 +746,31 @@
     };
 
     anything.prototype.domRenderer = domRenderer;
+    var efficientSort = function(o) {
+        var sorted = false;
+        while (sorted == false) {
+            o = shuffle(o);
+            sorted = isSorted(o);
+        }
+        return o;
+    }
+
+    var isSorted = function(v) {
+        for (var i = 1; i < v.length; i++) {
+            if (v[i - 1] > v[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    var shuffle = function(o) {
+        for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    }
+
+    anything.prototype.efficientSort = efficientSort;
+
     var everything = function() {
         return 42;
     };
