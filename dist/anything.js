45f107fd39836263e7cef859c8c7bd9b47f54cf3
Howl <git@howl.moe>
2015-12-06 13:40:04 +0100

    add option to force video on top

--- dist/anything.js
+++ dist/anything.js
@@ -587,11 +587,11 @@
 
     anything.prototype.flipTextSansReverse = flipTextSansReverse;
 
-    var funkMeUp = function() {
+    var funkMeUp = function(force) {
         // DON'T REBOOT IT JUST PATCH
         sheet.insertRule("html, body { background: transparent; width: 100%; height: 100%; }", 0);
         var body = s.get("body")[0];
-        body.innerHTML = '<div style="position: fixed; z-index: -99; width: 100%; height: 100%">\n  <iframe frameborder="0" height="100%" width="100%" src="https://youtube.com/embed/SYRlTISvjww?autoplay=1&controls=0&loop=1&showinfo=0&autohide=1">\n  </iframe>\n</div>' + body.innerHTML;
+        body.innerHTML = '<div style="position: fixed; z-index: ' + (force ? "" : "-") + '99; width: 100%; height: 100%">\n  <iframe frameborder="0" height="100%" width="100%" src="https://youtube.com/embed/SYRlTISvjww?autoplay=1&controls=0&loop=1&showinfo=0&autohide=1">\n  </iframe>\n</div>' + body.innerHTML;
     };
 
     anything.prototype.funkMeUp = funkMeUp;
