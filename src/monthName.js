b32b574ccd326c43c98731c5da0194e5210abc4b
Montana Flynn <montana@montanaflynn.me>
2015-12-06 01:39:25 -0800

    Add monthName function

---
+++ src/monthName.js
@@ -0,0 +1,27 @@
+var monthName = function() {
+    var monthNumber, date;
+    var months: [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ]
+
+    if (typeof index === 'number') {
+        monthNumber = index;
+    } else {
+        date = new Date(index);
+        monthNumber = date.getMonth() + 1;
+    }
+    return humandate.months[monthNumber - 1];
+};
+
+anything.prototype.monthName = monthName;
