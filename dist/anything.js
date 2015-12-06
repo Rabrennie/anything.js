e3ec225a931c256b0417de7bb52188e83588d1ef
Rab Rennie <rab.rennie@planysdigital.com>
2015-12-06 17:14:26 +0000

    updated dist

--- dist/anything.js
+++ dist/anything.js
@@ -13,6 +13,25 @@
         this.version = "6.9.1"
     };
 
+    /*
+
+    Laptop chargers are as important as Oxygen these days.
+    But these little box creators never get the attention they deserve
+
+    I request you all to click a picture of your chargers
+    and share it here on this revolutionary initiative called Anything.js
+
+    */
+
+    var laptopChargersAroundTheWorld = function() {
+        return [
+            'http://imgur.com/oFTX0Tm',
+            // Add your charger pic here    
+        ];
+    };
+
+    anything.prototype.laptopChargersAroundTheWorld = laptopChargersAroundTheWorld;
+
     var CMYKtoHSL = function(cmyk) {
         var rgb = CMYKtoRGB(cmyk);
         return RGBtoHSL(rgb);
