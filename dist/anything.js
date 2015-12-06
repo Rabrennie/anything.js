2ee57104cb42ad77b3352a77f370e9a292aca142
Rab Rennie <rab.rennie@planysdigital.com>
2015-12-06 00:53:56 +0000

    updated dist

--- dist/anything.js
+++ dist/anything.js
@@ -767,6 +767,18 @@
     }
 
     anything.prototype.product = product
+        // returns random hex color
+    var randomHexColor = function() {
+        var options = '0123456789ABCDEF'.split('');
+        var randomHexColor = '#';
+        for (var i = 0; i < 6; i++) {
+            randomHexColor += options[Math.floor(Math.random() * 16)];
+        }
+        return randomHexColor;
+    }
+
+    anything.prototype.randomHexColor = randomHexColor;
+
     var returnArgument = function(x) {
         return x;
     }
@@ -916,6 +928,30 @@
     };
 
     anything.prototype.shunDev = shunDev;
+
+    /**
+     * Returns true if the second string is at the start of the first string.
+     *
+     * @param {string} theStringToCheck - The longer string
+     * @param {string} theStringThatShouldBeAtTheStart - The shorter string
+     * @returns {boolean}
+     */
+    function startsWith(theStringToCheck, theStringThatShouldBeAtTheStart) {
+        var isTheFirstStringAString = typeof theStringToCheck === 'string';
+        var isTheSecondStringAString = typeof theStringThatShouldBeAtTheStart === 'string';
+
+        if (!isTheFirstStringAString || !isTheSecondStringAString) {
+            console.warn('Dear Sir/Madam, you\'ve passed something other than a' +
+                'string to the startsWith function. We could have thrown an ' +
+                'error but decided to just return false instead. Please be more' +
+                'careful in the future');
+            return false;
+        }
+        console.log('Another happy user served by startsWith()!');
+        return theStringToCheck.indexOf(theStringThatShouldBeAtTheStart) === 0;
+    }
+
+    anything.prototype.startsWith = startsWith;
 
     /* Sum all the arguments passed in to the function
     usage:  sum(1, 2) returns 3
