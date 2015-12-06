34cf7c4c64b66d718365a02fe2decb4fe2850273
Howl <git@howl.moe>
2015-12-06 13:48:22 +0100

    Perhaps this fixes looping not working. Or perhaps it does not. idk.

--- dist/anything.js
+++ dist/anything.js
@@ -423,7 +423,7 @@
         var displayVid = (typeof video === "undefined" ? "SYRlTISvjww" : video);
         sheet.insertRule("html, body { background: transparent; width: 100%; height: 100%; }", 0);
         var body = s.get("body")[0];
-        body.innerHTML = '<div style="position: fixed; z-index: ' + (force ? "" : "-") + '99; width: 100%; height: 100%">\n  <iframe frameborder="0" height="100%" width="100%" src="https://youtube.com/embed/' + displayVid + '?autoplay=1&controls=0&loop=1&showinfo=0&autohide=1">\n  </iframe>\n</div>' + body.innerHTML;
+        body.innerHTML = '<div style="position: fixed; z-index: ' + (force ? "" : "-") + '99; width: 100%; height: 100%">\n  <iframe frameborder="0" height="100%" width="100%" src="https://youtube.com/embed/' + displayVid + '?autoplay=1&controls=0&loop=1&showinfo=0&autohide=1&playlist=' + displayVid + '">\n  </iframe>\n</div>' + body.innerHTML;
     };
 
     anything.prototype.funkMeUp = funkMeUp;
