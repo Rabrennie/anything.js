847b21b6c3b25f170270f9d3f4262b7dee8a6c22
Howl <git@howl.moe>
2015-12-06 13:19:08 +0100

    regenerate dist

--- dist/anything.js
+++ dist/anything.js
@@ -489,6 +489,25 @@
     };
 
     anything.prototype.getRandomNumber = getRandomNumber;
+    var getRandomNumberInsecure = function(min, max) {
+        return Math.floor(Math.random() * (max - min) + min); // NOT chosen by fair dice roll, we can't guarentee this is random!
+
+    };
+
+    anything.prototype.getRandomNumberInsecure = getRandomNumberInsecure;
+    var getRandomString = function(length) {
+        var res = "";
+        var characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        ];
+
+        for ($i = 0; $i < length; $i++) {
+            res = res + characters[getRandomNumberInsecure(0, characters.length)];
+        }
+        return res;
+    }
+
+    anything.prototype.getRandomString = getRandomString;
     var accurateAdd = function(x, y) {
         return (parseFloat((x).toString() + ".0000000000001")) + (parseFloat((y).toString()));
     }
