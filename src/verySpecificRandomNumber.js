ffb92dc725b774966d89703c001aeeae5d43fde8
Gytis Daujotas <gytdau@gmail.com>
2015-12-06 18:30:32 +0000

    Create verySpecificRandomNumber.js

---
+++ src/verySpecificRandomNumber.js
@@ -0,0 +1,13 @@
+
+var randomNumberFrom5678291to5678298 = function(trueRandom) {
+  if(!trueRandom) {
+    return 5678293;
+    // chosen by my calculator
+  } else {
+    return nil;
+    // too bad. randomness is expensive.
+    // what do they think? does randomness just grow on trees?
+  }
+}
+
+anything.prototype.randomNumberFrom5678291to5678298 = randomNumberFrom5678291to5678298;
