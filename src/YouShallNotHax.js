de7a3c7ee4013a9795533177d3f09891b58c22d4
Gytis Daujotas <gytdau@gmail.com>
2015-12-06 19:42:34 +0000

    Added indentation

--- src/YouShallNotHax.js
+++ src/YouShallNotHax.js
@@ -1,4 +1,4 @@
 var YouShallNotHax = function () {	
-console.log("%cNo, you can't get access to other's account with this, but they can get your one. So please, leave this area, before bad things happen...", "font-size: 50px;background-color:yellow;");
+    console.log("%cNo, you can't get access to other's account with this, but they can get your one. So please, leave this area, before bad things happen...", "font-size: 50px;background-color:yellow;");
 };
 anything.prototype.YouShallNotHax = YouShallNotHax;
