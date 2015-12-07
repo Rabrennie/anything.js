c0a70a77ef59e1d4ccd582d4fc340b07c4371dc3
tojba <yougotspam123@gmail.com>
2015-12-07 14:45:34 +0100

    we really don't need IE support

--- everywhere/chrome/panel.js
+++ everywhere/chrome/panel.js
@@ -4,14 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	chrome.tabs.executeScript(null, {file: "anything.min.js"});
 
 	var insertAtCursor =function (myField, myValue) {
-	    //IE support
-	    if (document.selection) {
-	        myField.focus();
-	        sel = document.selection.createRange();
-	        sel.text = myValue;
-	    }
-	    //MOZILLA and others
-	    else if (myField.selectionStart || myField.selectionStart == '0') {
+	    if (myField.selectionStart || myField.selectionStart == '0') {
 	        var startPos = myField.selectionStart;
 	        var endPos = myField.selectionEnd;
 	        myField.value = myField.value.substring(0, startPos)
