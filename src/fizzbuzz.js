4aee12886ef3a94789ca3701dd738af8500f131a
Max Glenister <omgmog@users.noreply.github.com>
2015-12-07 11:51:21 +0000

    🌝

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,15 +1,13 @@
-var fizzbuzz = function () {
+var fizz = 'Fizz';
+var buzz = 'Buzz';
+var _fizzbuzz = function (i) {
     // fire an ultra-difficult algorithm to either print Fizz, Buzz, FizzBuzz or the number
-    for (var i = 1; i <= 100; i++) {
-        if (i % 15 == 0) {
-            console.log("FizzBuzz");
-        } else if (i % 5 == 0) {
-            console.log("Buzz");
-        } else if (i % 3 == 0) {
-            console.log("Fizz");
-        } else {
-            console.log(i);
-        }
+    return (i % 15 === 0 ? fizz + buzz : (i % 5 === 0 ? buzz : (i % 3 === 0 ? fizz : i)));
+};
+
+var fizzbuzz = function () {
+    for (var i = 0; i <= 100; i++) {
+        console.log(_fizzbuzz(i));
     }
 };
 
