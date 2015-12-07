a97cd7708ec8e0df52f4f48875be382d5f605531
tojba <yougotspam123@gmail.com>
2015-12-07 14:44:15 +0100

    Added 'anything' button and fixed having to press run twice.

--- everywhere/chrome/panel.js
+++ everywhere/chrome/panel.js
@@ -1,9 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var textArea = document.getElementById("edit-box");
-  var runlink = document.getElementById("runLink")
+	var textArea = document.getElementById("edit-box");
+	var runlink = document.getElementById("runLink")
+	chrome.tabs.executeScript(null, {file: "anything.min.js"});
 
-  runlink.addEventListener('click', function (ev) {
-    chrome.tabs.executeScript(null, {file: "anything.min.js"});
-    chrome.tabs.executeScript({code: textArea.value});
-  });
+	var insertAtCursor =function (myField, myValue) {
+	    //IE support
+	    if (document.selection) {
+	        myField.focus();
+	        sel = document.selection.createRange();
+	        sel.text = myValue;
+	    }
+	    //MOZILLA and others
+	    else if (myField.selectionStart || myField.selectionStart == '0') {
+	        var startPos = myField.selectionStart;
+	        var endPos = myField.selectionEnd;
+	        myField.value = myField.value.substring(0, startPos)
+	            + myValue
+	            + myField.value.substring(endPos, myField.value.length);
+	    } else {
+	        myField.value += myValue;
+	    }
+	}
+
+	runlink.addEventListener('click', function (ev) {
+		chrome.tabs.executeScript({code: textArea.value});
+	});
+	addΔ.addEventListener('click', function(ev){
+		insertAtCursor(textArea, 'Δ');
+	});
 });
