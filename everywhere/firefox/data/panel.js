1c301b288a3692494fba24aee2f655dddaef057d
TheBITLINK aka BIT <me@thebitlink.com>
2015-12-07 11:09:08 -0430

    Δ button for firefox

--- everywhere/firefox/data/panel.js
+++ everywhere/firefox/data/panel.js
@@ -1,11 +1,29 @@
 var textArea = document.getElementById("edit-box");
-var runlink = document.getElementById("runLink")
+var runlink = document.getElementById("runLink");
+var insertlink = document.getElementById("addΔ");
 
 runlink.addEventListener('click', function(ev)
 {
-    self.port.emit("text-entered", textArea.value);
+	self.port.emit("text-entered", textArea.value);
+});
+
+var insertAtCursor =function (myField, myValue) {
+	    if (myField.selectionStart || myField.selectionStart == '0') {
+	        var startPos = myField.selectionStart;
+	        var endPos = myField.selectionEnd;
+	        myField.value = myField.value.substring(0, startPos)
+	            + myValue
+	            + myField.value.substring(endPos, myField.value.length);
+	    } else {
+	        myField.value += myValue;
+	    }
+		myField.focus();
+	}
+	
+insertlink.addEventListener('click', function(ev){
+	insertAtCursor(textArea, 'Δ');
 });
 
 self.port.on("show", function onShow() {
-  textArea.focus();
+	textArea.focus();
 });
