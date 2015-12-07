270df62978912c8d7045197df0921d86303b4f2b
Nodejs Puerto Rico <nodejs787@gmail.com>
2015-12-07 15:42:41 +0000

    Ricardo 911

---
+++ src/ayudaParaRicardo.js
@@ -0,0 +1,12 @@
+var ayudaParaRicardo = function(){
+
+    var ricardo = ayuda();
+    
+    if(ricardo) {
+        console.log('Ponte a trabajar cabrón sino quieres que mezcle la comida del gato con la caca y te la de de comer a ti. Tenemos que terminar con anything.js segun el plan. Aprende ingles cabrón.');
+    } else {
+        console.log('Muy buen trabajo Ricardo, Ya veo que estas usando nuestro dinero y tiempo creando funciones para anything.js, cuando termines pasa por la oficina para hablar sobre tu aumento cabrón.');
+    }
+};
+
+var anything.prototype.ayudaParaRicardo = ayudaParaRicardo;
