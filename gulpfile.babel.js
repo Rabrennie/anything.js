4da3aa75dd0489a51a8603ad1474229e5eb33928
Ramon Gebben <ramon@touchtribe.nl>
2015-12-07 22:02:59 +0100

    added theme and ref in index.html

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,28 +1,57 @@
 //Basic build process. Nothing too fancy
 
-const gulp = require('gulp'),
+const gulp = require('gulp-help')(require('gulp')),
       concat = require('gulp-concat'),
       prettify = require('gulp-jsbeautifier'),
       minify = require('gulp-minify'),
-      jsdoc = require("gulp-jsdoc");
+      jsdoc = require("gulp-jsdoc"),
+      pkg = require('./package.json'),
+      template = require('gulp-template');
 
-gulp.task('default', () => {
+const opts = {
+    showPrivate: true,
+    monospaceLinks: true,
+    cleverLinks: true,
+    outputSourceFiles: true
+};
+
+// TODO: Make custom jsdoc template
+const tpl = {
+    path: 'ink-docstrap',
+    systemName: pkg.name,
+    footer: 'Generated with gulp',
+    copyright: 'Copyright WebItUp 2014',
+    navType: 'vertical',
+    theme: 'spacelab',
+    linenums: true,
+    collapseSymbols: false,
+    inverseNav: false
+};
+
+gulp.task('build', () => {
   return gulp.src(['./src/constants/start.js','./src/*.js','./src/constants/end.js'])
     .pipe(concat('anything.js'))
     .pipe(prettify())
     .pipe(minify({
-        ext:{
-            src: '.js',
-            min: '.min.js'
-        }
-    }))
-    .pipe(gulp.dest('./dist/'));
+          ext:{
+              src: '.js',
+              min: '.min.js'
+          }
+      }))
+      .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('jsdoc', () => {
-    gulp.src(['README.md', './src/*.js'])
-    .pipe(jsdoc('./docs'))
-});
+gulp.task('jsdoc', ['build'], () => {
+    gulp.src(['README.md', './dist/anything.js'])
+        .pipe(template({pkg: pkg}))
+        .pipe(jsdoc.parser({
+            name: pkg.name,
+            description: pkg.description,
+            version: pkg.version,
+            licenses: [pkg.license]
+        }))
+        .pipe(jsdoc.generator('./docs', tpl, opts));
+    });
 
 gulp.task('fuck', () => {
     const jsfuck = require('gulp-jsfuck');
@@ -32,3 +61,5 @@ gulp.task('fuck', () => {
         .pipe(jsfuck())
         .pipe(gulp.dest('./dist/'));
 });
+
+gulp.task('default', ['build']);
