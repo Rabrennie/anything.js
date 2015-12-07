0a34579fe851d8a9522531df1af9a6ba08954060
tojba <yougotspam123@gmail.com>
2015-12-07 18:05:50 +0100

    Save current text

--- everywhere/chrome/panel.js
+++ everywhere/chrome/panel.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
 	var textArea = document.getElementById("edit-box");
-	var runlink = document.getElementById("runLink")
+	var runlink = document.getElementById("runLink");
+	var timeout = 0;
+	var addΔ = document.getElementById("addΔ");
 	chrome.tabs.executeScript(null, {file: "anything.min.js"});
+	chrome.storage.local.get("textArea", function(result){
+		textArea.value = result;
+		console.log(result);
+	});
 
-	var insertAtCursor =function (myField, myValue) {
+	var insertAtCursor = function (myField, myValue) {
 	    if (myField.selectionStart || myField.selectionStart == '0') {
 	        var startPos = myField.selectionStart;
 	        var endPos = myField.selectionEnd;
@@ -17,8 +23,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	runlink.addEventListener('click', function (ev) {
 		chrome.tabs.executeScript({code: textArea.value});
+		ev.preventDefault();
+		console.log("running");
+		console.log("");
 	});
-	addΔ.addEventListener('click', function(ev){
+	addΔ.addEventListener('click', function (ev) {
 		insertAtCursor(textArea, 'Δ');
+	});
+	textArea.addEventListener('keyup', function () {
+		timeout = setTimeout(function() {
+			console.log("saving");
+			chrome.storage.local.set({"textArea": textArea.value}, function() {
+				alert("saved");
+			});
+		}, 200);
+		clearTimeout(timeout);
 	});
 });
