9cec999923acdd34d1d99fa7df0bff295c836196
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2015-12-08 13:32:01 +0100

    Convert tabs to spaces

--- src/getAwesomePeople.js
+++ src/getAwesomePeople.js
@@ -2,25 +2,25 @@
 * getAwesomePeople
 */
 var getAwesomePeople = function(success, error) {
-	var httpRequest;
+    var httpRequest;
 
-	function makeRequest(url) {
-		httpRequest = new XMLHttpRequest();
-		httpRequest.onreadystatechange = handleCallbacks;
-		httpRequest.open('GET', url);
-		httpRequest.send();
-	}
+    function makeRequest(url) {
+        httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = handleCallbacks;
+        httpRequest.open('GET', url);
+        httpRequest.send();
+    }
 
-	function handleCallbacks() {
-		if (httpRequest.readyState === XMLHttpRequest.DONE) {
-			if (httpRequest.status === 200) {
-				console.log(httpRequest.responseText);
-				success(httpRequest.responseText);
-			} else {
-				error(httpRequest.responseText);
-			}
-		}
-	}
+    function handleCallbacks() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                console.log(httpRequest.responseText);
+                success(httpRequest.responseText);
+            } else {
+                error(httpRequest.responseText);
+            }
+        }
+    }
 
-	makeRequest('https://api.github.com/repos/Rabrennie/anything.js/contributors');
+    makeRequest('https://api.github.com/repos/Rabrennie/anything.js/contributors');
 };
