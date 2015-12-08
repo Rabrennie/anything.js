950d012a39f1ad8f10ceace0753f3a5cf147ec15
Travis CI <rennierab@gmail.com>
2015-12-08 09:34:24 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -2474,6 +2474,24 @@ function _typeof(obj) {
     };
 
     anything.prototype.greetHannah = greetHannah;
+    /**
+     * Generates a GUID
+     */
+    var guid = function guid() {
+        var d = new Date().getTime();
+        if (window.performance && typeof window.performance.now === "function") {
+            d += performance.now(); //use high-precision timer if available
+        }
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c == 'x' ? r : r & 0x3 | 0x8).toString(16);
+        });
+        return uuid;
+    };
+
+    anything.prototype.guid = guid;
+
     var hash = function hash(message) {
         return message.length.toString(16);
     };
