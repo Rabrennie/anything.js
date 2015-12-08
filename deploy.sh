904ffcdad7c759490d09e6dabee58fc0ddc391dd
Rab Rennie <rab.rennie@planysdigital.com>
2015-12-08 01:52:56 +0000

    added deploy for travis

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+
+# inside this git repo we'll pretend to be a new user
+git config user.name "Travis CI"
+git config user.email "<you>@<your-email>"
+
+# The first and only commit to this new Git repo contains all the
+# files present with the commit message "Deploy to GitHub Pages".
+git add ./dist/*
+git commit -m "Update dist"
+
+# Force push from the current repo's master branch to the remote
+# repo's gh-pages branch. (All previous history on the gh-pages branch
+# will be lost, since we are overwriting it.) We redirect any output to
+# /dev/null to hide any sensitive credential data that might otherwise be exposed.
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" origin master > /dev/null 2>&1
