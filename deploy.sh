86022970f652f1ceb4c50fdff44504cbd53535e2
rabrennie <rennierab@gmail.com>
2015-12-08 02:20:16 +0000

    Updated deploy

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,9 @@ set -e # exit with nonzero exit code if anything fails
 git config user.name "Travis CI"
 git config user.email "rennierab@gmail.com"
 
-# The first and only commit to this new Git repo contains all the
-# files present with the commit message "Deploy to GitHub Pages".
+git checkout master
 git add dist/anything.js
 git add dist/anything.min.js
 git commit -m "Update dist"
 
-# Force push from the current repo's master branch to the remote
-# repo's gh-pages branch. (All previous history on the gh-pages branch
-# will be lost, since we are overwriting it.) We redirect any output to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
 git push "https://${GITHUB_SECRET_TOKEN}@${GH_REF}" master > /dev/null 2>&1
