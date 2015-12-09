102f19c06231e339b6c95ef151867d6b4b3de30b
Travis CI <rennierab@gmail.com>
2015-12-09 06:43:01 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -614,6 +614,44 @@ function _typeof(obj) {
 
     anything.prototype.arrayContains = arrayContains;
 
+    /**
+     * @desc Turn your browser into an asteroids game
+     * @author Yehya Awad <yehyaawad.kip@gmail.com>
+     */
+    var asteroids = function asteroids() {
+        // asteroids/Kickassgame
+        // Credit goes to http://kickassapp.com/ for the game code
+        (function() {
+            var s = document.createElement('script');
+            s.type = 'text/javascript';
+            s.src = 'http://hi.kickassapp.com/kickass.js';
+            document.body.appendChild(s);
+        })();
+
+        // destroy image
+        var destroy = function destroy() {
+            var ad = document.querySelector("#kickass-hello-sunshine");
+            if (ad != null) {
+                ad.setAttribute("src", "");
+                clearInterval(interv);
+                //destroyed
+            }
+        };
+        var interv = setInterval(destroy, 1000);
+    };
+
+    /**
+     * @desc Exit asteroids game (you could also exit with ESC)
+     * @author Yehya Awad <yehyaawad.kip@gmail.com>
+     */
+    var asteroidsExit = function asteroidsExit() {
+        if (typeof window.KICKASSGAME.destroy === "undefined") {} else {
+            window.KICKASSGAME.destroy();
+        }
+    };
+
+    anything.prototype.asteroids = asteroids;
+    anything.prototype.asteroidsExit = asteroidsExit;
     var async_doThingTomorrow = function async_doThingTomorrow(cb) {
         var now = new Date();
         var tomorrow = new Date();
