89e5fe3e101eac57bd711ac6274496cfca1e2fa0
Vitor Cortez <vitor.a.cortez@gmail.com>
2015-12-10 10:03:36 -0300

    Add `isX[Smaller|Bigger]ThanOrEqualsToY` functions
    Now checking if a number is smaller than or bigger than, or equals to
    another number is a piece o cake!
    WARNING: THIS FUNCTION MAY OR MAY NOT WORK WITH FLOATS.

---
+++ src/isXBiggerOrSmallerThanOrEqualsToY.js
@@ -0,0 +1,30 @@
+/**
+ * Don't do "x <= y", just do "x < (y + 1)".
+ * @param {number} x
+ * @param {number} y
+ * @see {@link http://redd.it/3rc2rm}
+ */
+var isXSmallerThanOrEqualsToY = function(x, y) {
+    if (x < (y + 1) ? true : false) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+/**
+ * Readability at its finest.
+ * @param {number} x
+ * @param {number} y
+ * @see {@link isXSmallerrThanOrEqualToY} for further information.
+ */
+var isXBiggerThanOrEqualsToY = function(x, y) {
+    if (x > (y + 1) ? true : false) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+anything.prototype.isXSmallerThanOrEqualToY = isXSmallerThanOrEqualToY;
+anything.prototype.isXBiggerThanOrEqualToY = isXBiggerThanOrEqualToY;
