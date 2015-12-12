fcc0cbb71094cded550a46b1c96207de54de1816
Vitor Cortez <vitor.a.cortez@gmail.com>
2015-12-12 16:41:32 -0300

    Add `printTwelveDaysOfTesting` function. Ho Ho Ho
    On the first day of testing, my QA pointed out to me: There's a
    duplicate in the source tree.

---
+++ src/printTwelveDaysOfTesting.js
@@ -0,0 +1,45 @@
+/**
+ * The Twelve Days of Christmas Testing.
+ * @see {@link http://redd.it/3wh7rv}
+ */
+var printTDoT = function() {
+    var days = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth'];
+
+    var gifts = [
+        "An Irreproducible bug",
+        "Two Build requests",
+        "Three Small defects",
+        "Four Creeping specs",
+        "FIVE CRITICALS",
+        "Six DBs failing",
+        "Seven Customers cursing",
+        "Eight Hours wasted",
+        "Nine Testers testing",
+        "Ten Tables dropping",
+        "Eleven Coders coding",
+        "Twelve Patches pending"
+    ];
+
+    var verses = [],
+        song;
+
+    for (var i = 0; i < 12; ++i) {
+        var lines = [];
+        lines[0] = `On the ${days[i]} day of Testing, my QA gave to me:`;
+
+        var j = i + 1,
+            k = 0;
+
+        while (j-- > 0)
+            lines[++k] = `- ${gifts[j]}${j == 0 ? '.': ','}`;
+
+        verses[i] = lines.join('\n');
+
+        if (i == 0)
+            gifts[0] = "And a Bug that can't be reproduced";
+    }
+
+    console.log(verses.join('\n\n'));
+};
+
+anything.prototype.printTwelveDaysOfTesting = printTDoT;
