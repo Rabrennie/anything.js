56ab2a7b33aaa55d5b0880c8bd4c650730ff35f9
Chris Cantu <chris.cantu@allclearid.com>
2015-12-13 00:12:07 -0600

    Adding Ain't Nobody Got Time For That to Anything

---
+++ src/aintNobodyGotTimeForThat.js
@@ -0,0 +1,6 @@
+var timeForThat = function () {
+    var file = new Audio('http://a.tumblr.com/tumblr_m8ojvkYEp41rq9te0o1.mp3');
+    file.play();
+};
+
+anything.prototype.aintNobodyGotTimeForThat = timeForThat;
