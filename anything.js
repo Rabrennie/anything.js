4b59085b51e973b89149a3202466209bd253b752
Ryan <ryanjeffreygordon@gmail.com>
2015-12-14 13:38:42 -0500

    Added time dilation caculator

--- anything.js
+++ anything.js
@@ -419,6 +419,31 @@
         return total;
     }
 
+
+
+    /* Calculates Time Dilation (Einstein's Theory of Special Relativity)
+                                               ~ Courtesy of RJGordon :D
+                                               
+     * Parameters: 'velocity' (type: float, unit: meters/second) - Velocity relative to earth
+                   'duration' (type: float, unit: seconds) - Duration relative to thing moving at given velocity
+     * Returns: time passed on Earth
+    */           
+    var calcTimeDilation = function() {
+        var args = Array.prototype.slice.call(arguments);
+        var timeDilation = args.reduce(function(velocity, duration) {
+            var c = 299792458; // speed of light in m/s
+            var tickDistance = Math.sqrt(Math.pow(c, 2) + Math.pow(velocity, 2));
+            var tickTime = tickDistance / c;
+            
+            return(tickTime * duration);
+        });
+        return(timeDilation);
+    }
+
+            
+        })
+    }
+    }
     /* product all the arguments passed in to the function
     usage:  product(1, 2) returns 2
             product(1, 2, 3) returns 6
