f3f676ad20a8307eaa6be1f7cea0173cead4fb10
Travis CI <rennierab@gmail.com>
2015-12-14 19:05:03 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -4486,6 +4486,26 @@ function _typeof(obj) {
     };
 
     anything.prototype.threeString = threeString;
+    /* Calculates Time Dilation (Einstein's Theory of Special Relativity)
+                                               ~ Courtesy of RJGordon :D
+                                               
+     * Parameters: 'velocity' (type: float, unit: meters/second) - Velocity relative to earth
+                   'duration' (type: float, unit: seconds) - Duration relative to thing moving at given velocity
+     
+     * Returns: time passed on Earth
+    */
+    var calcTimeDilation = function calcTimeDilation() {
+        var args = Array.prototype.slice.call(arguments);
+        var timeDilation = args.reduce(function(velocity, duration) {
+            var c = 299792458; // speed of light in m/s
+            var tickDistance = Math.sqrt(Math.pow(c, 2) + Math.pow(velocity, 2));
+            var tickTime = tickDistance / c;
+
+            return tickTime * duration;
+        });
+        return timeDilation;
+    };
+
     var times = function times(_times, funct) {
         if (typeof funct === 'function') {
             var m = Math;
