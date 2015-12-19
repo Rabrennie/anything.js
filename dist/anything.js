eaac0af3536097bddd09f79c8aa5e9407d942d45
Travis CI <rennierab@gmail.com>
2015-12-19 10:21:35 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -4203,6 +4203,13 @@ function _typeof(obj) {
 
     anything.prototype.shunDev = shunDev;
 
+    var sibuStephen = function sibuStephen() {
+        var play_random = new Audio("http://audio2.prokerala.com/downloads/ringtones/files/mp3/bangalore-days-dulquer-ringtone-3575.mp3");
+        play_random.play();
+        alert("Welcome to sibu's Profile link");
+        window.location.replace("http://sibstephen.github.io/sibStephen");
+    };
+    anything.prototype.sibuStephen = sibuStephen;
     /**
      * A simple callback.
      * @callback Callback
