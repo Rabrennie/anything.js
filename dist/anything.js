1ddbdf4bab24c92db1952eb449831fee66330707
Travis CI <rennierab@gmail.com>
2015-12-20 18:39:24 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -4166,6 +4166,18 @@ function _typeof(obj) {
     })();
 
     anything.prototype.sheet = sheet;
+    /**
+     * Mai Shuffle Algorithm
+     *  > because Knuth-Fisher-Yates is too hard to pronounce
+     *
+     * Note: shuffles in place, so the original array gets modified.
+     */
+    function MaiShuffle(o) {
+        for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x) {}
+        return o;
+    }
+
+    anything.prototype.shuffle = MaiShuffle;
     var shunDev = function shunDev() {
 
         var self = this;
