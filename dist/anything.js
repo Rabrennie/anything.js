f765652f19151f7b4f93f7b8c35e5d24827bdb85
Travis CI <rennierab@gmail.com>
2015-12-21 17:51:28 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -3812,6 +3812,34 @@ function _typeof(obj) {
     };
 
     anything.prototype.play_pingpong = play_pingpong;
+
+    /* a really dumb prefix notation calculator */
+    var pncalc = function pncalc(s) {
+        var calculate = function calculate(t, start) {
+            switch (t[start]) {
+                case "+":
+                    return calculate(t, start + 1) + calculate(t, start + 2);
+                    break;
+                case "-":
+                    return calculate(t, start + 1) - calculate(t, start + 2);
+                    break;
+                case "*":
+                    return calculate(t, start + 1) * calculate(t, start + 2);
+                    break;
+                case "/":
+                    return calculate(t, start + 1) / calculate(t, start + 2);
+                    break;
+                default:
+                    return Number(t[start]);
+                    break;
+            }
+        };
+        var t = s.split(" ");
+        return calculate(t, 0, t.length);
+    };
+
+    anything.prototype.pncalc = pncalc;
+
     var pong = function pong() {
         return "ping";
     };
