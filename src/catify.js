6f389a1c645243e49336ff293d99803ab73e2422
Denton-L <liu.denton+github@gmail.com>
2015-12-23 12:10:10 -0500

    Added catify.js

---
+++ src/catify.js
@@ -0,0 +1,7 @@
+function catify(){
+		var imgs = document.getElementsByTagName("img");
+		for (var i = 0; i < imgs.length; i++) {
+				var img = imgs[i];
+				img.src="http://placekitten.com/"+img.clientWidth+"/"+img.clientHeight;
+		}
+}
