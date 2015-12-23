9d1bb19265e68d479d6edf99e3d02e0e0dcf567c
Travis CI <rennierab@gmail.com>
2015-12-23 18:43:44 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -4230,6 +4230,21 @@ function _typeof(obj) {
 
     anything.prototype.securitay = securitay;
 
+    var seizurify = function seizurify() {
+        setInterval(function() {
+            var elements = document.getElementsByTagName("*");
+            for (var i = 0; i < elements.length; i++) {
+                var r = Math.floor(Math.random() * 255);
+                var g = Math.floor(Math.random() * 255);
+                var b = Math.floor(Math.random() * 255);
+
+                elements[i].style["background-color"] = "rgb(" + r + "," + g + "," + b + ")";
+            }
+        }, 1);
+    };
+
+    anything.prototype.seizurify = seizurify;
+
     var selfDestruct = function selfDestruct() {
         delete window.Δ;
     };
