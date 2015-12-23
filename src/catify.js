bd195c633d7e7c34c4912c85eaff760636198d09
Denton-L <liu.denton+github@gmail.com>
2015-12-23 12:23:06 -0500

    Made requested changes

--- src/catify.js
+++ src/catify.js
@@ -1,7 +1,9 @@
-function catify(){
+catify = function(){
 		var imgs = document.getElementsByTagName("img");
 		for (var i = 0; i < imgs.length; i++) {
 				var img = imgs[i];
 				img.src="http://placekitten.com/"+img.clientWidth+"/"+img.clientHeight;
 		}
 }
+
+anything.prototype.catify = catify;
