e6c77446f954494eb9407178815cc1275c863e01
Vitor Cortez <vitoracortez+github@gmail.com>
2015-12-24 11:40:26 -0300

    Update deploy script to update gh-pages
    This is an attempt to update the gh-pages branch using
    the `git stash` command to share the updated `dist/` folder.

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,17 @@ git config user.name "Travis CI"
 git config user.email "rennierab@gmail.com"
 
 git checkout master
+git stash
+git stash apply
+git add dist/anything.js
+git add dist/anything.min.js
+git commit -m "Update dist [ci skip]"
+
+git checkout gh-pages
+git stash pop
 git add dist/anything.js
 git add dist/anything.min.js
 git commit -m "Update dist [ci skip]"
 
 git push "https://${GITHUB_SECRET_TOKEN}@${GH_REF}" master > /dev/null 2>&1
+git push "https://${GITHUB_SECRET_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
