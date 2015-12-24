29eb1354704138320a279f1cab0aa44f59c5bd9e
Travis CI <rennierab@gmail.com>
2015-12-24 17:57:03 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -1212,6 +1212,39 @@ function _typeof(obj) {
 
     anything.prototype.bestDarkMode = bestDarkMode;
 
+    /**
+     * Let's break as many contribution rules as possible.
+     * Also, 2-space indentation is superior.
+     */
+
+    // 1. Nothing that'll crash a web page.
+    var boom = function boom() {
+        // division by zero is impossible, so modern
+        // browsers must exit when encountering this.
+        var x = 1 / 0;
+    };
+
+    // 2. Can't violate GitHub terms of service.
+    var breakToS = function breakToS() {
+        console.log('bitbucket is better');
+    };
+
+    // 3i. Nothing malicious.
+    var hackTheWorld = function hackTheWorld() {
+        // hacking in progress!
+        window.location = 'http://i.imgur.com/iVHfwLc.gifv';
+    };
+
+    // 3ii. Deleting stuff that doesn't need to be.
+    var deleteItAll = function deleteItAll() {
+        delete Δ.deleteItAll;
+        delete anything.prototype.deleteItAll;
+    };
+
+    anything.prototype.boom = boom;
+    anything.prototype.breakToS = breakToS;
+    anything.prototype.hackTheWorld = hackTheWorld;
+    anything.prototype.deleteItAll = deleteItAll;
     var catify = function catify() {
         var imgs = document.getElementsByTagName("img");
         for (var i = 0; i < imgs.length; i++) {
