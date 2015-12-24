9715a92b0d2696645784b9e7a5349a6be5f98ba0
Xor Boole <mcyoung@mit.edu>
2015-12-24 13:00:48 -0500

    Add betterVersion

---
+++ src/betterVersion.js
@@ -0,0 +1,7 @@
+/**
+ * Given that this is an advanced, webscale framework, 
+ * the version should be bumped as often as possible.
+ */
+
+anything.prototype.realVersion = anything.prototype.version;
+anything.prototype.version += "-" + Date.now();
