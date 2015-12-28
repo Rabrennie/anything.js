c22c34aa9d291d75e0795fdb6e95e46f79d48801
Travis CI <rennierab@gmail.com>
2015-12-28 22:26:32 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -456,6 +456,18 @@ function _typeof(obj) {
 
     anything.prototype.SHOUTDOM = SHOUTDOM;
 
+    var SongThatNeverEnds = function SongThatNeverEnds(i) {
+        while (i > 0) {
+            console.log("This is the song that never ends");
+            console.log("It just goes on and on my friend");
+            console.log("Some people started singing it not knowing what it was");
+            console.log("And they continued singing it forever just because");
+            i = i - 1;
+        }
+    };
+
+    anything.prototype.SongThatNeverEnds = SongThatNeverEnds;
+
     var YouShallNotHax = function YouShallNotHax() {
         console.log("%cNo, you can't get access to other's account with this, but they can get your one. So please, leave this area, before bad things happen...", "font-size: 50px;background-color:yellow;");
     };
