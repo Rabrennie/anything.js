de6677a2bf0ba16a42e30b8cf1182a759bd977f9
Travis CI <rennierab@gmail.com>
2016-01-01 03:37:15 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -3254,6 +3254,27 @@ function _typeof(obj) {
 
     anything.prototype.guid = guid;
 
+    /**
+     * Happy New Years!
+     * If you can't tell, I'm not a javascript developer.
+     * This was inferred knowledge from other languages and stack overflow.
+     * God help us all.
+     */
+
+    var happyNewYear = function happyNewYear() {
+        var todaysDate = new Date().getFullYear();
+        if (todaysDate == "2016") {
+            console.log("Happy New Years!");
+            console.log("Here's to making 2016 awesome!");
+            alert("Happy New Years!");
+            alert("Here's to making 2016 awesome!");
+        } else {
+            console.log("Either it's broken or it's not 2016 yet!");
+        }
+    };
+
+    anything.prototype.happyNewYear = happyNewYear;
+
     var harlemShake = function harlemShake() {
         function c() {
             var e = document.createElement("link");
