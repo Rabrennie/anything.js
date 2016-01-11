e4498a7fef7995a041d0df8d60c87268b824e02c
Gytis Daujotas <gytdau@gmail.com>
2016-01-11 20:43:59 +0000

    Added rip.js

---
+++ src/rip.js
@@ -0,0 +1,19 @@
+/*
+
+        _|_
+   ______|_____
+  /             \
+  | R.I.P.      |
+  | Anything.js |
+  |             |
+  | "It was     |
+  | nice while  |
+  | it lasted"  |
+  | - gytdau    | o
+D,|,,,,,,,,,,,,,|,/,
+
+*/
+
+/*
+If you'd like, you can add your condolences to the tombstone.
+*/
