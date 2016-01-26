0203ab08363ef2d73d631107f8e5cb66ed86caea
Patrick Ambli <patrick.ambli@gmail.com>
2016-01-26 12:12:23 -0600

    RAINBOW.js
    Adds rainbows to any HTML element. (Don't question it)

---
+++ src/RAINBOW.js
@@ -0,0 +1,5 @@
+var RAINBOW = function(element, speed){
+  var i = 0;
+  speed = (typeof speed != 'undefined') ? speed: 5;
+  return setInterval(function(){element.style.background='hsl('+i+',100%, 50%)';i=(i <= 359)?i+1:0;}, speed)
+}
