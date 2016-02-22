71c1ced63887db88b75c85fbd9fb4e922cf194e6
Christian Varisco <vari.chri1993@gmail.com>
2016-02-22 21:28:59 +0100

    If you are bored

---
+++ src/imbored.js
@@ -0,0 +1,8 @@
+// If you are bored, close this 7 popup :)
+var imBored = function(){
+    for(var i=0; i<7; i++){
+        alert('If you are bored close this popup :) '+ (7-i))
+    }
+};
+
+anything.prototype.imBored = imBored;
