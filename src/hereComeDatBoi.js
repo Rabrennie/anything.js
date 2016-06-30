87da5a3ac5f64b90156588a635d9412387e8d50c
Vitor Cortez <vitoracortez+github@gmail.com>
2016-06-30 19:07:07 -0300

    Add `hereComeDatBoi` function
    O shit waddup!

---
+++ src/hereComeDatBoi.js
@@ -0,0 +1,8 @@
+/**
+ * Here comes that boy.
+ * @return {string} That boy's remarkable response.
+ * @author Vitor Cortez <vitoracortez+github@gmail.com>
+ */
+var hereComeDatBoi = function() {
+  return "o shit waddup!";
+};
