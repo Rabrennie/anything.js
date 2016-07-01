01daf92bce4e5fed89a326fe33e46f44dd1e7612
Travis CI <rennierab@gmail.com>
2016-07-01 18:12:26 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -25,6 +25,12 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
         this.version = "3";
     };
 
+    var MostOnTop = function MostOnTop() {
+        return Δ.SHOUT("Top Dog on Top.");
+    };
+
+    anything.prototype.MostOnTop = MostOnTop;
+
     var moreOnTop = function moreOnTop() {
         return Δ.reverse("In your face, InderpalPfeffis! I'm the top one now!");
     };
@@ -1343,7 +1349,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
         var imgs = document.getElementsByTagName("img");
         for (var i = 0; i < imgs.length; i++) {
             var img = imgs[i];
-            img.src = "http://placekitten.com/" + img.clientWidth + "/" + img.clientHeight;
+            img.src = "http://baconmockup.com/" + img.clientWidth + "/" + img.clientHeight;
         }
     };
 
@@ -3460,6 +3466,17 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     };
 
     anything.prototype.helloWorld = helloWorld;
+
+    /**
+     * Here comes that boy.
+     * @return {string} That boy's remarkable response.
+     * @author Vitor Cortez <vitoracortez+github@gmail.com>
+     */
+    var hereComeDatBoi = function hereComeDatBoi() {
+        return "o shit waddup!";
+    };
+
+    anything.prototype.hereComeDatBoi = hereComeDatBoi;
 
     var hohoho = function hohoho(context) {
 
@@ -5875,6 +5892,31 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     };
 
     anything.prototype.tellMeImCool = tellMeImCool;
+    /**
+     * Gives some thanks to the big man.
+     * @return {Abstract} Joy
+     * @author Vitor Cortez <vitoracortez+github@gmail.com>
+     */
+    function thanksMrPresident() {
+        var titles = document.createElement('article');
+        titles.id = 'obamatitle';
+        var content = document.createElement('section');
+        content.id = 'obamacontent';
+        var message = document.createElement('p');
+
+        document.styleSheets[0].insertRule('#obamatitle {position:absolute; width:18em; height: 50em; bottom: 0; left: 50%; margin-left: -9em; font-size: 350%; font-weight: bold; text-align: justify; overflow: hidden; transform-origin: 50% 100%; transform: perspective(300px) rotateX(25deg);}', 0);
+        document.styleSheets[0].insertRule("#obamatitle::after {position: absolute; content: ' '; left: 0; right: 0; top: 0; bottom:60%; background-image: linear-gradient(top, rgba(0,0,0,1) 0%, transparent 100%); pointer-events:none;}", 0);
+        document.styleSheets[0].insertRule('#obamacontent {position:absolute; top:100%; animation: scroll 100s linear 4s infinite;}', 0);
+        document.styleSheets[0].insertRule('@keyframes scroll { 0% {top:100%;} 100% {top:-170%;}}', 0);
+
+        message.textContent = 'Thanks Obama '.repeat(100);
+        content.appendChild(message);
+        titles.appendChild(content);
+        document.body.appendChild(titles);
+    };
+
+    anything.prototype.ThanksMrPresident = thanksMrPresident;
+
     /**
      * @author HartgerV
      * @name thatsMyJam
