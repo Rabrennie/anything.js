4811cd05ff8dcc5e41eb5962874803ba7e93b389
Samuel Kelemen <SCKelemen@users.noreply.github.com>
2016-06-30 22:19:41 -0400

    Grammar Fixes

--- src/SongThatNeverEnds.js
+++ src/SongThatNeverEnds.js
@@ -1,10 +1,6 @@
 var SongThatNeverEnds = function(i){
-	while (i>0) {
-	console.log("This is the song that never ends");
-	console.log("It just goes on and on my friend");
-	console.log("Some people started singing it not knowing what it was");
-	console.log("And they continued singing it forever just because");
- 	 i=i-1;
+	while (true) {
+	console.log("This is the song that never ends. \r\n It just goes on and on, my friend. \r\n Some people started singing it, not knowing what it was. \r\n And they continued singing it, forever, just because!");
 	}
 }
 
