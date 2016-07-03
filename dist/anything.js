0f036f444be2da2549af7e3587d0a4e93d6cd7e1
Travis CI <rennierab@gmail.com>
2016-07-03 01:52:08 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -1586,6 +1586,57 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     };
 
     /**
+     * Inspired by http://imgur.com/jalm9B8
+     * @param object - The object to convert.
+     */
+    var convertToJSON = function convertToJSON(object) {
+        var json_str = '';
+        // Primitives
+        switch (typeof object === "undefined" ? "undefined" : _typeof(object)) {
+            case 'string':
+                return '"' + object + '"';
+            case 'boolean':
+            case 'number':
+                return object.toString();
+            case 'undefined':
+            case 'function':
+                return false;
+        }
+        // null
+        if (object == null) return 'null';
+        // Arrays
+        if ((typeof object === "undefined" ? "undefined" : _typeof(object)) == 'object' && object.forEach) {
+            json_str += '[';
+            object.forEach(function(obj) {
+                json_str += convertToJSON(obj); // Recursive is the future!
+                json_str += ',';
+            });
+            // Remove trailing comma
+            json_str = json_str.substr(0, json_str.length - 1);
+            return json_str + ']';
+        }
+        // Objects
+        else if ((typeof object === "undefined" ? "undefined" : _typeof(object)) == 'object') {
+            json_str = '{';
+            // Get those keys
+            var keys = Object.keys(object);
+            // Build JSON
+            keys.forEach(function(key) {
+                json_str += '"' + key + '":';
+                json_str += convertToJSON(object[key]); // Recursive is amazing!
+                json_str += ',';
+            });
+            // Remove trailing comma
+            json_str = json_str.substr(0, json_str.length - 1);
+            return json_str + '}';
+        } else {
+            return 'wtf';
+        }
+    };
+
+    anything.prototype.convertToJSON = convertToJSON;
+
+    /**
      * Safely encode your string data to prevent hackers.
      * @param {string} stringData - String to be protected.
      * @return {string} Your data is now protected from anonymous.
