ea359576cde98f249d6fcad21db69db8f00037d6
Travis CI <rennierab@gmail.com>
2016-07-06 22:35:48 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -5857,6 +5857,31 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
     anything.prototype.sleepSort = sleepSort;
 
+    /**
+     * Sorts an array in a slow fashion.
+     * @param {Array} A - The array to be slowly sorted.
+     * @param {number} i - the letter 'i'.
+     * @param {number} j - that black guy's name from MIB.
+     * @author Vitor Cortez <vitoracortez+github@gmail.com>
+     * @see {@link http://www.mipmip.org/tidbits/pasa.pdf}
+     */
+    var slowSort = function slowSort(A, i, j) {
+        if (i >= j) return;
+        else {
+            var m = Math.floor((i + j) / 2);
+            slowSort(A, i, m);
+            slowSort(A, m + 1, j);
+            if (A[m] > A[j]) {
+                var temp = A[m];
+                A[m] = A[j];
+                A[j] = temp;
+            }
+            slowSort(A, i, j - 1);
+        }
+    };
+
+    anything.prototype.slowSort = slowSort;
+
     Object.defineProperty(anything, 'something', {
         get: function get() {
             var keys = Object.keys(window);
