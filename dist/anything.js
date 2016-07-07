740a816d81be6ee02def04370b61f1889a1f4205
Travis CI <rennierab@gmail.com>
2016-07-07 22:43:48 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -1586,6 +1586,24 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     };
 
     /**
+     * Converts a JS boolean to a C boolean.
+     * @param {boolean} bool - a JS boolean.
+     * @return {number} C boolean.
+     * @author Vitor Cortez <vitoracortez+github@gmail.com>
+     */
+    var convertJSBooleanToCBoolean = function convertJSBooleanToCBoolean(bool) {
+        if (bool === false && bool !== true) {
+            return 0;
+        } else if (bool === true && bool !== false) {
+            return 1;
+        } else {
+            return Infinity;
+        }
+    };
+
+    anything.prototype.convertJSBooleanToCBoolean = convertJSBooleanToCBoolean;
+
+    /**
      * Inspired by http://imgur.com/jalm9B8
      * @param object - The object to convert.
      */
