08ae57f5a6841cdc5a24c9b62d93d704ec5f1a39
Vitor Cortez <vitoracortez+github@gmail.com>
2016-07-08 23:07:02 -0300

    Add `NotNotFound` method
    This is not not found.

---
+++ src/NotNotFound.js
@@ -0,0 +1,9 @@
+/**
+ * It is not not found.
+ * @author Vitor Cortez <vitoracortez+github@gmail.com>
+ */
+var ImNotNotFound = function() {
+  console.log('This is not not found.');
+};
+
+anything.prototype.NotNotFound = ImNotNotFound;
