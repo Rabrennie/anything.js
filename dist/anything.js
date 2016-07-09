961d37a1582cde0011a44c37ff2a19474c266f3f
Travis CI <rennierab@gmail.com>
2016-07-09 23:24:08 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -396,6 +396,29 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     anything.prototype.HowToMakeSausageRice = sausageRiceAlgorithm;
 
     /**
+     * Prints a random programmer joke.
+     * @author Vitor Cortez <vitoracortez+github@gmail.com>
+     */
+    var jokeMeUpBoy = function jokeMeUpBoy() {
+        var jokes = getListOfJokes();
+        var rand = Math.floor(Math.random() * jokes.length);
+        console.log(jokes[rand]);
+    };
+
+    /**
+     * Holds a list of classic programmer jokes.
+     * Feel free to add a new joke each day.
+     * @private
+     */
+    function getListOfJokes() {
+        var arr = [];
+        arr.push('Q: How do you tell an introverted computer scientist from an extroverted computer scientist?\nA: An extroverted computer scientist looks at your shoes when he talks to you.');
+        return arr;
+    }
+
+    anything.prototype.JokeMeUpBoy = jokeMeUpBoy;
+
+    /**
      * It is not not found.
      * @author Vitor Cortez <vitoracortez+github@gmail.com>
      */
