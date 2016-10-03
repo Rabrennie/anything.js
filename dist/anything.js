868b7c7d6121b0d8484b46176fa6a8a2c72a3e70
Travis CI <rennierab@gmail.com>
2016-10-03 03:35:00 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -5712,6 +5712,24 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
     anything.prototype.rotatePage = rotatePage;
 
+    // Returns a rubber duck & its motivating words.
+    // Made by CharmaineLee
+
+    var rubberDuckMe = function rubberDuckMe() {
+        // rubber ducky's motivating words
+        var debug = ["Talk me through your code line by line.", "I'm here for you.", "Give it another thought.", "I see a problem with that line over there."];
+        return debug[Math.floor(Math.random() * debug.length)];
+
+        // changes images on page to mr duck (only icon as of right now)
+        var images = document.getElementsByTagName("img");
+        for (var i = 0; i < images.length; i++) {
+            var img = images[i];
+            img.src = "http://cliparts.co/cliparts/6iy/5y5/6iy5y58KT.png" + img.clientWidth + "/" + img.clientHeight;
+        }
+    };
+
+    anything.prototype.rubberDuckMe = rubberDuckMe;
+
     // jquery-like simple dom wrapper.
     var s = {
         get: function get(selector) {
