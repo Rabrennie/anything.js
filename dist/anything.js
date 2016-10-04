96688cbbd22486670d6902837e3651f9e23d0cba
Travis CI <rennierab@gmail.com>
2016-10-04 08:58:53 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -5712,14 +5712,24 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
     anything.prototype.rotatePage = rotatePage;
 
-    // Returns a rubber duck & its motivating words.
+    // Returns a rubber duck's motivating words - optimized for rubber duck debugging
     // Made by CharmaineLee
 
     var rubberDuckMe = function rubberDuckMe() {
-        // rubber ducky's motivating words
-        var debug = ["Talk me through your code line by line.", "I'm here for you.", "Give it another thought.", "I see a problem with that line over there."];
-        return debug[Math.floor(Math.random() * debug.length)];
+        var duckQuotes = getDuckQuotes();
+        var rand = Math.floor(Math.random() * duckQuotes.length);
+        console.log(duckQuotes[rand]);
     };
+
+    // rubber ducky's motivating words
+    function getDuckQuotes() {
+        var debug = [];
+        debug.push('Talk me through your code line by line.');
+        debug.push("I'm here for you.");
+        debug.push('Give it another thought.');
+        debug.push('I see a problem with that line over there.');
+        return debug;
+    }
 
     anything.prototype.rubberDuckMe = rubberDuckMe;
 
