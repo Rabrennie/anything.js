f20d6f4515be9f80f6dd171a9ae75777d5f02e5a
Travis CI <rennierab@gmail.com>
2016-10-27 15:28:20 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -4798,6 +4798,20 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
     anything.prototype.nothing = nothing;
     /**
+     * Returns the number of letters in a word
+     * @param {String} The word to test.
+     * @returns {Number} The number of letters in the word 
+     */
+    var numberOfLettersInTheWord = function numberOfLettersInTheWord(word) {
+        var numberOfLetters = 0;
+        if (typeof word === 'string') {
+            return (word.match(/[a-zA-Z]/g) || []).length;
+        }
+        return numberOfLetters;
+    };
+    anything.prototype.numberOfLettersInTheWord = numberOfLettersInTheWord;
+
+    /**
      * A function for generating the number of letters in the word "cat"
      * @returns {Number} The number of letters in the word "cat"
      */
