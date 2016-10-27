32d4f8b5074016ef098cb7d17569eef6d975e926
gsimpson <gs@bn.co>
2016-10-27 08:26:03 -0400

    Adhering to 4 spaces indentation from guidelines.

--- src/numberOfLettersInTheWord.js
+++ src/numberOfLettersInTheWord.js
@@ -3,10 +3,10 @@
  * @returns {Number} The number of letters in the word "cat"
  */
 var numberOfLettersInTheWord = function(word) {
-  var numberOfLetters = 0;
-  if (typeof word === 'string') {
-    return (word.match(/[a-zA-Z]/g) || []).length;
-  }
-  return numberOfLetters;
+    var numberOfLetters = 0;
+    if (typeof word === 'string') {
+        return (word.match(/[a-zA-Z]/g) || []).length;
+    }
+    return numberOfLetters;
 }
 anything.prototype.numberOfLettersInTheWord = numberOfLettersInTheWord;
