d17ac9c63baeccd1f4cf132bbd2c1f409e503d5f
Travis CI <rennierab@gmail.com>
2016-12-01 08:33:27 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -2987,6 +2987,22 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     };
 
     anything.prototype.generateUniqueColorHue = generateUniqueColorHue;
+    /*
+    Will replace all weird char to dash (-)
+    */
+    var generateUrlFriendlyString = function generateUrlFriendlyString(name) {
+        name = $.trim(name.toLowerCase());
+        name = name.replace(/[^A-Za-z0-9]+/g, "-");
+        while (name.charAt(0) === '-') {
+            name = name.substr(1);
+        }
+        while (name.charAt(name.length - 1) === '-') {
+            name = name.substr(0, name.length - 1);
+        }
+        return name;
+    };
+
+    anything.prototype.generateUrlFriendlyString = generateUrlFriendlyString;
     /* you will definitely need this */
     var getAnimalNoise = function getAnimalNoise() {
         var all = ["arrooff", "arf", "aroo", "awk", "baa", "baraag", "beep", "bzzz", "caw", "chirp", "chirr", "cluck", "coo", "cuckcoo", "eeeaaah", "meow", "moo", "neigh", "oink", "ook", "owoooah", "purr", "quack", "squawk", "squeak", "woof"];
