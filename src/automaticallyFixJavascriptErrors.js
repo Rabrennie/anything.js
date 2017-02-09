a12ab47d707aaf86140591d2ad4ec68454b6dd12
Samuel Kelemen <SCKelemen@users.noreply.github.com>
2017-02-08 18:15:50 -0600

    Create automaticallyFixJavascriptErrors.js

---
+++ src/automaticallyFixJavascriptErrors.js
@@ -0,0 +1,5 @@
+var automaticallyFixJavascriptErrors = function () {
+    window.onerror = function() { return false; };
+ };
+ 
+ anything.prototype.automaticallyFixJavascriptErrors = automaticallyFixJavascriptErrors;
