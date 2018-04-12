2f79ea9f805f3499f9172d3bbacc8bcb82dfb587
Travis CI <rennierab@gmail.com>
2018-04-12 21:03:52 +0000

    Update dist [ci skip]

--- dist/anything.js
+++ dist/anything.js
@@ -1547,6 +1547,35 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
     anything.realVersion = anything.prototype.version;
     anything.version += "-" + Date.now();
+
+    var bottlesOfBeer = function bottlesOfBeer(numBottles) {
+        var bottles = numBottles;
+        if (isNaN(bottles) || bottles < 1) {
+            bottles = 99;
+        }
+
+        while (bottles >= 0) {
+            if (bottles == 1) {
+                console.log("One bottle of beer on the wall.");
+                console.log("One bottle of beer.");
+            } else {
+                console.log(bottles + " bottles of beer on the wall.");
+                console.log(bottles + " bottles of beer.");
+            }
+            console.log("Take one down.");
+            console.log("Pass it around.");
+            bottles--;
+            if (bottles == 1) {
+                console.log("One bottle of beer on the wall.");
+                bottles = 99;
+            } else {
+                console.log(bottles + " bottles of beer on the wall.");
+            }
+        }
+    };
+
+    anything.prototype.bottlesOfBeer = bottlesOfBeer;
+
     /**
      * Let's break as many contribution rules as possible.
      * Also, 2-space indentation is superior.
